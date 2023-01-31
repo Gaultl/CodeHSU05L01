d660a0af6383feab8882fc8c255d8dccf405cef0
gaultl <gaultl@wlhs.wlwv.k12.or.us>
2023-01-31 09:23:16 -0800

    Added default constructor and overloaded constructors

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -2,11 +2,26 @@ import java.text.DecimalFormat;
 public class Rectangle { //make public for CS A
 
     //instance variables
+    private double width;
+    private double height;
+    private double perimeter;
+    private double area;
 
     //default constructor (make public for CS A)
-
+    public Rectangle(){
+        width = 1.0;
+        height = 1.0;
+    }
 
     //overloaded constructor (with parameters)
+    public Rectangle(double h, double w){
+        width = w;
+        height = h;
+    }
+
+    public Rectangle(double width){
+        this.width = width;
+    }
 
 
 
